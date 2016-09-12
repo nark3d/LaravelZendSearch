86467030378a60330829be75cd10aa6ebd4cddc2
Adam Lewis <adam.lewis@bestservedcold.com>
2016-09-12 11:03:01 +0100

    Adding in codesniffers and CI

--- tests/Laravel/SearchTest.php
+++ tests/Laravel/SearchTest.php
@@ -9,6 +9,7 @@ class SearchTest extends TestCase
     public function setUp()
     {
         parent::setUp();
+
         // @todo set up mock index object
         // @todo set up mock query object
         // @todo set up mock Dummy Model object and add it via model()
